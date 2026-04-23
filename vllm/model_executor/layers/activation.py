33ff26bf5f4d8292de63b408ecd2ed1412dc3823
Alex <alex.tech.lab@outlook.com>
2026-04-23 15:26:15 +0800

    rm self op
    Signed-off-by: Alex <alex.tech.lab@outlook.com>

--- vllm/model_executor/layers/activation.py
+++ vllm/model_executor/layers/activation.py
@@ -291,11 +291,6 @@ class GeluAndMul(CustomOp):
         self.approximate = approximate
         if approximate not in ("none", "tanh"):
             raise ValueError(f"Unknown approximate mode: {approximate}")
-        if current_platform.is_cuda_alike() or current_platform.is_xpu():
-            if approximate == "none":
-                self.op = torch.ops._C.gelu_and_mul
-            elif approximate == "tanh":
-                self.op = torch.ops._C.gelu_tanh_and_mul
         if current_platform.is_rocm() and approximate == "tanh":
             logger.warning_once(
                 "[ROCm] PyTorch's native GELU with tanh approximation is unstable "
